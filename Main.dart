fc2bdd0afb58271bb7ec3fdc47ac57ac515a4501
AS-DB <adityasagarofficial2005@gmail.com>
2025-10-13 23:50:09 +0530

    Maps

--- Main.dart
+++ Main.dart
@@ -1,14 +1,2 @@
-//Dart Maps
-void main() {
-  //declaration of map variable
-
-  var cars = {"Tesla": "Electric", "Toyato": "Gasoline"};
-  print(cars);
-
-  var fruits = new Map();
-  fruits['apple'] = 'red';
-  print(fruits);
-
-  //to print specific value
-  print(cars["Tesla"]);
-}
+//Dart List
+void main() {}
