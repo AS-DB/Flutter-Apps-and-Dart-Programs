b6465a641034896013e7167e5bdc80fc69b0461a
AS-DB <adityasagarofficial2005@gmail.com>
2025-10-13 23:13:37 +0530

    Looping

--- Main.dart
+++ Main.dart
@@ -1,26 +1,27 @@
-//Dart Decision Statement
+//Dart looping
 void main() {
-  //if-else
-  var num1 = 21;
-  var num2 = 31;
-
-  if (num1 > num2) {
-    print("$num1 is great");
-  } else if (num1 == num2) {
-    print("$num1 is equal to other num also");
-  } else {
-    print("$num2 is greater then other");
+  //for loop
+  for (int i = 0; i < 10; i++) {
+    print("hello");
   }
 
-  var day = "Sunday";
-  switch (day) {
-    case "Sunday":
-      print("Its the funny day");
-      break;
-    case "Monday":
-      print("Its not a funday");
-      break;
-    default:
-      print("No day is like exist");
+  //while
+  int i = 0;
+  while (i < 9) {
+    print("hello adi");
+    i++;
+  }
+
+  //do-while
+  var i1 = 2;
+  do {
+    print("jo jo");
+    i1++;
+  } while (i1 < -1);
+
+  //iterate over list
+  List a1 = ['a', 'b', 'c', 'd'];
+  for (var c in a1) {
+    print(c);
   }
 }
