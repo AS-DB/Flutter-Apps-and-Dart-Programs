f81c3f73ffc2d53c5d13d27664e5cec060f876be
AS-DB <adityasagarofficial2005@gmail.com>
2025-10-13 21:30:24 +0530

    all types Variable declaration

--- Main.dart
+++ Main.dart
@@ -1,6 +1,18 @@
 //variables
+import 'dart:ffi';
+
 void main() {
   //variable can declare with var,val,const,final
   const a = "aditya";
   print(a);
+  //variable can also can define in a way of datatype variable name
+  int a1 = 21;
+  print(a1);
+  //null safety
+  String? a2;
+  print(a2);
+  //late variable
+  late String name;
+  name = "aditya";
+  print(name);
 }
