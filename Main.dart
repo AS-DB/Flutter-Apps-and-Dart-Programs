4c3b8445cedf59b4b868e0f503259485b01d6e79
AS-DB <adityasagarofficial2005@gmail.com>
2025-10-14 07:47:05 +0530

    Dart future and async and await

--- Main.dart
+++ Main.dart
@@ -1,2 +1,12 @@
-//Dart List
-void main() {}
+//Dart future and async and await
+void main() async {
+  //futureFunction();
+
+  await futureFunction();
+  print("Hello");
+}
+
+Future<void> futureFunction() async {
+  await Future.delayed(Duration(seconds: 5));
+  print("hello");
+}
