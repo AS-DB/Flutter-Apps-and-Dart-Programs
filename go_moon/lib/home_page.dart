d52d767bacdeb5223b527eb6d251452abeae017c
AS-DB <adityasagarofficial2005@gmail.com>
2025-10-16 19:26:12 +0530

    CLean code and using the Text widget for static data for our app with textstyle and SafeArea

--- go_moon/lib/home_page.dart
+++ go_moon/lib/home_page.dart
@@ -5,7 +5,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: _pagetitle());
+    return Scaffold(body: SafeArea(child: _pagetitle()));
   }
 
   Widget _pagetitle() {
