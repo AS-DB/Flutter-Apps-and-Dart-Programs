e8e7d0de987334457a3611182a0014ac91bc2386
AS-DB <adityasagarofficial2005@gmail.com>
2025-10-20 22:44:10 +0530

    Working With ListView and ListTile Widget

--- task_lee/lib/pages/home_page.dart
+++ task_lee/lib/pages/home_page.dart
@@ -21,8 +21,24 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: Colors.red,
         toolbarHeight: _deviceHeight * 0.15,
-        title: Text("Taskly", style: TextStyle(fontSize: 10)),
+        title: Text("Taskly", style: TextStyle(fontSize: 15)),
       ),
+      body: _taskList(),
+    );
+  }
+
+  Widget _taskList() {
+    return ListView(
+      children: [
+        ListTile(
+          title: const Text(
+            "DO YOGA",
+            style: TextStyle(decoration: TextDecoration.lineThrough),
+          ),
+          subtitle: Text(DateTime.now().toString()),
+          trailing: Icon(Icons.check_box_outlined, color: Colors.red),
+        ),
+      ],
     );
   }
 }
