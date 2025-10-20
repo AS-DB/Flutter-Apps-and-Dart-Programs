62069d2949785d897e9109c62292e90750985d29
AS-DB <adityasagarofficial2005@gmail.com>
2025-10-20 22:12:25 +0530

    App bar for taskly

---
+++ task_lee/lib/pages/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  _HomePageState();
+
+  late double _deviceHeight, _deviceWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        toolbarHeight: _deviceHeight * 0.15,
+        title: Text("Taskly", style: TextStyle(fontSize: 10)),
+      ),
+    );
+  }
+}
